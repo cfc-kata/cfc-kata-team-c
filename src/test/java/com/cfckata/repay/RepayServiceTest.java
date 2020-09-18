14b4ea19e9e0dfa9668ca588d14e964bf473025c
menglei <menglei@hnzycfc.com>
2020-09-18 08:41:36 +0800

    测试jar包更换成org.junit.Test

--- src/test/java/com/cfckata/repay/RepayServiceTest.java
+++ src/test/java/com/cfckata/repay/RepayServiceTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.Before;
 //import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.BeforeEach;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
